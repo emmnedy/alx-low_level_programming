ff8eb77cdb2e084056506391272b63a82a804bb3
NUEL <emmnedy@gmail.com>
2023-07-17 11:18:43 -0700

    dog h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,12 @@
 #ifndef STRUCT_DOG_H
 #define STRUCT_DOG_H
 
+/**
+ * struct dog - create  structure dog
+ * @name: var for data type char*
+ * @float: var for data type age
+ * @owner: var for data type char* 2
+ */ 
 
 struct dog 
 {
